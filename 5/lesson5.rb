1d9ad7073771db0087e419567b5eb275c53464f3
tackwan <129726011+tackwan@users.noreply.github.com>
2023-04-08 09:35:32 +0000

    [finish]7章

---
+++ 5/lesson5.rb
@@ -0,0 +1,9 @@
+total_prince = 150
+
+if total_prince > 100
+  puts "みかんを購入。所持金に余りあり。"
+elsif total_prince == 100
+  puts "みかんを購入。所持金は0円。"
+else
+  puts "みかんを購入することができません。"
+end
---
+++ 6/dice.rb
@@ -0,0 +1,6 @@
+dice = 0
+
+while dice != 6 do    #diceが6でない間繰り返し処理
+   dice = rand(1..6)  #1~6の数字がランダムに出力される
+  puts dice
+end
